bfd9a7cb14e293f503477438ef74641c07cffc3e
Jordan Quartey <jordev14@gmail.com>
2024-08-09 08:41:56 +0100

    style nav buttons

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,115 +1,130 @@
-import { View } from '@/types'
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+} from "lucide-react";
 
-import styles from './Navigation.module.scss'
+import { View } from "@/types";
+
+import styles from "./Navigation.module.scss";
 
 interface NavigationProps {
-	view: View
-	setView: (view: View) => void
-	setButtonClicked: (button: string) => void
+  view: View;
+  setView: (view: View) => void;
+  setButtonClicked: (button: string) => void;
 }
 
 export const Navigation = ({
-	view,
-	setView,
-	setButtonClicked,
+  view,
+  setView,
+  setButtonClicked,
 }: NavigationProps): JSX.Element => {
-	const handleClick = (button: string) => {
-		const viewMap: Record<View, Record<string, View>> = {
-			home: {
-				up: 'feed',
-				down: 'projects',
-				left: 'about',
-				right: 'blog',
-			},
-			about: {
-				right: 'home',
-			},
-			projects: {
-				up: 'home',
-			},
-			feed: {
-				down: 'home',
-			},
-			blog: {
-				left: 'home',
-			},
-		}
+  const handleClick = (button: string) => {
+    const viewMap: Record<View, Record<string, View>> = {
+      home: {
+        up: "feed",
+        down: "projects",
+        left: "about",
+        right: "blog",
+      },
+      about: {
+        right: "home",
+      },
+      projects: {
+        up: "home",
+      },
+      feed: {
+        down: "home",
+      },
+      blog: {
+        left: "home",
+      },
+    };
 
-		const nextView = viewMap[view][button]
-		if (nextView) {
-			setView(nextView)
-		}
-		setButtonClicked(button)
-	}
+    const nextView = viewMap[view][button];
+    if (nextView) {
+      setView(nextView);
+    }
+    setButtonClicked(button);
+  };
 
-	switch (view) {
-		case 'feed':
-			return (
-				<button
-					className={`${styles.down} ${styles.button}`}
-					onClick={() => handleClick('down')}
-				>
-					Down
-				</button>
-			)
+  switch (view) {
+    case "feed":
+      return (
+        <button
+          className={`${styles.down} ${styles.button}`}
+          onClick={() => handleClick("down")}
+        >
+          <p>Home</p>
+          <ChevronDown />
+        </button>
+      );
 
-		case 'blog':
-			return (
-				<button
-					className={`${styles.left} ${styles.button}`}
-					onClick={() => handleClick('left')}
-				>
-					Left
-				</button>
-			)
+    case "blog":
+      return (
+        <button
+          className={`${styles.left} ${styles.button}`}
+          onClick={() => handleClick("left")}
+        >
+          <ChevronLeft />
+          <p>Home</p>
+        </button>
+      );
 
-		case 'projects':
-			return (
-				<button
-					className={`${styles.up} ${styles.button}`}
-					onClick={() => handleClick('up')}
-				>
-					Up
-				</button>
-			)
+    case "projects":
+      return (
+        <button
+          className={`${styles.up} ${styles.button}`}
+          onClick={() => handleClick("up")}
+        >
+          <p>Home</p>
+          <ChevronUp />
+        </button>
+      );
 
-		case 'about':
-			return (
-				<button
-					className={`${styles.right} ${styles.button}`}
-					onClick={() => handleClick('right')}
-				>
-					Right
-				</button>
-			)
-		default:
-			return (
-				<>
-					<button
-						className={`${styles.up} ${styles.button}`}
-						onClick={() => handleClick('up')}
-					>
-						Up
-					</button>
-					<button
-						className={`${styles.down} ${styles.button}`}
-						onClick={() => handleClick('down')}
-					>
-						Down
-					</button>
-					<button
-						className={`${styles.left} ${styles.button}`}
-						onClick={() => handleClick('left')}
-					>
-						Left
-					</button>
-					<button
-						className={`${styles.right} ${styles.button}`}
-						onClick={() => handleClick('right')}
-					>
-						Right
-					</button>
-				</>
-			)
-	}
-}
+    case "about":
+      return (
+        <button
+          className={`${styles.right} ${styles.button}`}
+          onClick={() => handleClick("right")}
+        >
+          <p>Home</p>
+          <ChevronRight />
+        </button>
+      );
+    default:
+      return (
+        <>
+          <button
+            className={`${styles.up} ${styles.button}`}
+            onClick={() => handleClick("up")}
+          >
+            <ChevronUp />
+            <p>Social Feed</p>
+          </button>
+          <button
+            className={`${styles.down} ${styles.button}`}
+            onClick={() => handleClick("down")}
+          >
+            <p>Projects</p>
+            <ChevronDown />
+          </button>
+          <button
+            className={`${styles.left} ${styles.button}`}
+            onClick={() => handleClick("left")}
+          >
+            <ChevronLeft />
+            <p>About</p>
+          </button>
+          <button
+            className={`${styles.right} ${styles.button}`}
+            onClick={() => handleClick("right")}
+          >
+            <p>Blog</p>
+            <ChevronRight />
+          </button>
+        </>
+      );
+  }
+};
